cd70f0f9175300b111a40315e8981324ad1485ea
liudong <a895011874@163.com>
2017-11-18 15:56:05 +0800

    Signed-off-by: liudong <a895011874@163.com>

---
+++ web/common/js/dataModel.js
@@ -0,0 +1,12 @@
+var dataModel={
+    'username':"admin",
+    content:[{
+        "titleName":"昨夜西风凋碧树，独上高楼，忘近天涯路",
+    "label":"诗词"
+},{
+    "titleName":"衣带渐宽终不悔，为伊消得人憔悴",
+    "label":"诗词"
+},{
+    "titleName":"众里寻他千百度，暮然回首，那人却在灯火阑珊处",
+    "label":"诗词"
+}]};
