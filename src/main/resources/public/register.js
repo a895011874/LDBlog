fc2aa5dc48f5f11b9d428d70f6df5aabdfc0503c
liudong <a895011874@163.com>
2017-11-22 22:05:59 +0800

    注册界面修改，添加校验，

--- src/main/resources/public/register.js
+++ src/main/resources/public/register.js
@@ -1,29 +1,28 @@
-var app = angular.module("app",[]);
-app.controller("registerController",function ($scope) {
-    $scope.register = function ($scope) {
-
-        var username = $("#inputUsername").val();
-        var password = $("#inputPassword").val();
-        var repassword = $("#inputPasswordRe").val();
-        var phoneNumber = $("#inputPhoneNumber").val();
-        var email = $("#inputEmail").val();
+var app = angular.module("app", []);
+app.controller("registerController", function ($scope) {
+    function register($scope) {
+        console.log(1212)
+        var username = $("#username").val();
+        var password = $("#password").val();
+        var repassword = $("#passwordre").val();
+        var phoneNumber = $("#phoneNumber").val();
+        var email = $("#email").val();
 
         userData = {
-            username:username,
-            password:password,
-            phone:phoneNumber,
-            email:email
+            username: username,
+            password: password,
+            phone: phoneNumber,
+            email: email
         };
-
         $.ajax({
             url: "/ldblog/user",
-            data:userData,
+            data: userData,
             dataType: "json",
             type: "post",
             async: false,
             success: function (data) {
                 resData = data;
-                if(data.state === "success") {
+                if (data.state === "success") {
                     window.location.href = "login.html";
                 }
                 console.log(data);
@@ -32,5 +31,69 @@ app.controller("registerController",function ($scope) {
                 console.log(result);
             }
         });
-    }
-});
+    };
+
+    /**
+     * 表单校验
+     */
+    setTimeout(function () {
+        $('form').bootstrapValidator({
+            message: 'This value is not valid',
+            feedbackIcons: {
+                valid: 'glyphicon glyphicon-ok',
+                invalid: 'glyphicon glyphicon-remove',
+                validating: 'glyphicon glyphicon-refresh'
+            },
+            submitHandler: function () {
+                console.log("submit")
+            },
+            fields: {
+                username: {
+                    message: '用户名验证失败',
+                    validators: {
+                        notEmpty: {
+                            message: '用户名不能为空'
+                        },
+                        stringLength: {
+                            max: 20,
+                            message: '用户名不能长于20个字符'
+                        }
+                    }
+                },
+                email: {
+                    validators: {
+                        notEmpty: {
+                            message: '邮箱地址不能为空'
+                        }
+                    }
+                },
+                password: {
+                    validators: {
+                        notEmpty: {
+                            message: '密码不能为空'
+                        },
+                        identical: {
+                            field: 'passwordre',
+                            message: '两次输入的密码不相符'
+                        }
+                    }
+                },
+                passwordre: {
+                    validators: {
+                        notEmpty: {
+                            message: '密码不能为空'
+                        },
+                        identical: {
+                            field: 'password',
+                            message: '两次输入的密码不相符'
+                        }
+                    }
+                }
+            }
+        }).on('success.form.bv', function () {
+            register();
+        });
+    });
+    20
+})
+
